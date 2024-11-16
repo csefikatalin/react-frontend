af83ae9047fa2b20aa9896e2c89c920a607af2df
cskati <csefi.katalin@gmail.com>
2024-11-16 08:42:16 +0100

    axios alap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [apiData, setApiData] = useState(null); 
+  function getData(vegpont, fv) {
+    axios
+      .get(vegpont,fv)
+      .then(function (response) {
+        // handle success
+        console.log(response.data)
+        fv(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      
+      })
+      .finally(function () {
+        // always executed
+      
+      });
+  }
+ 
+  useEffect(() => {
+    getData("http://127.0.0.1:8000/api/books", setApiData); 
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Axios 
       </header>
     </div>
   );
